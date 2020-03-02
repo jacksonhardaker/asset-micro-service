521ed8c3e11609202bd472d481f7e1d95b694a73
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-03-02 17:40:29 -0500

    add ability to resize with cover/contain

--- api/process.js
+++ api/process.js
@@ -3,20 +3,20 @@ const { send, load, resize, optimizeForWeb, blur } = require('../utils');
 
 module.exports = async (req, res) => {
   const { pathname } = parse(req.url);
-  const { src, w, h, q, b } = req.query;
+  const { src, w, h, q, b, cover, contain } = req.query;
   const path = src || pathname;
 
   const img = await load(path);
 
   if (img) {
     if (w || h)
-      await resize(img, w, h);
-  
+      await resize({ img, w, h, cover, contain });
+
     if (b)
       await blur(img, b);
-  
+
     await optimizeForWeb(img, q);
-  
+
     send(res, img);
   }
   else {
--- utils/resize.js
+++ utils/resize.js
@@ -1,5 +1,23 @@
 const Jimp = require("jimp");
 
-module.exports = async (img, w, h) => {
+const alignment = {
+  lt: Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_TOP,
+  lm: Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_MIDDLE,
+  lb: Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_BOTTOM,
+  ct: Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_TOP,
+  cm: Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE,
+  cb: Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_BOTTOM,
+  rt: Jimp.HORIZONTAL_ALIGN_RIGHT | Jimp.VERTICAL_ALIGN_TOP,
+  rm: Jimp.HORIZONTAL_ALIGN_RIGHT | Jimp.VERTICAL_ALIGN_MIDDLE,
+  rb: Jimp.HORIZONTAL_ALIGN_RIGHT | Jimp.VERTICAL_ALIGN_BOTTOM,
+};
+
+module.exports = async ({ img, w, h, cover, contain }) => {
+  if (w && h && cover)
+    return img.cover(+w, +h, alignment[cover])
+
+  if (w && h && contain)
+    return img.contain(+w, +h, alignment[cover])
+
   return img.resize(w ? +w : Jimp.AUTO, h ? +h : Jimp.AUTO);
 };
