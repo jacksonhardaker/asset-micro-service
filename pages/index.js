73c98c74b09be26ecd9c5e49bddad68d6d94b19c
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-08-24 08:12:05 -0400

    refactor: add title to head

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import { IndexPage } from "../components/IndexPage";
-import { AssetOptionsProvider } from "../context/AssetOptionsContext";
-import { ResultsProvider } from "../context/ResultsContext";
-import { LoadingProvider } from "../context/LoadingContext";
+import Head from 'next/head';
+import { IndexPage } from '../components/IndexPage';
+import { AssetOptionsProvider } from '../context/AssetOptionsContext';
+import { ResultsProvider } from '../context/ResultsContext';
+import { LoadingProvider } from '../context/LoadingContext';
 
 export default function Index() {
   return (
     <LoadingProvider>
       <ResultsProvider>
         <AssetOptionsProvider>
+          <Head>
+            <title>Asset Micro Service</title>
+          </Head>
           <IndexPage />
         </AssetOptionsProvider>
       </ResultsProvider>
