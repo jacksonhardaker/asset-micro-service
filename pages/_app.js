2567c32000035d516cb658b43180436540fa4c04
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-08-25 21:00:12 -0400

    add github corner

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import App from 'next/app'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { styletron, debug } from '../styletron'
+import { GithubCorner } from '../components/GitHubCorner'
 
 export default class _App extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
       <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
+        <GithubCorner />
         <main>
           <Component {...pageProps} />
         </main>
