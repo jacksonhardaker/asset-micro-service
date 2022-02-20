95b12006017d6a4112c7b343530b55618a8bc720
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-02-19 21:33:13 -0500

    add other processing abilities

--- pages/api/sharp.js
+++ pages/api/sharp.js
@@ -1,10 +1,22 @@
 import sharp from 'sharp';
 
 export default async (req, res) => {
-  const { src, w, h, q, b, cover, contain, raw } = req.query;
+  const { src, w, h, q, b, fit } = req.query;
+
+  if (fit &&
+    fit !== sharp.fit.contain &&
+    fit !== sharp.fit.cover &&
+    fit !== sharp.fit.fill &&
+    fit !== sharp.fit.inside &&
+    fit !== sharp.fit.outside
+  ) {
+    throw Error(`Invalid 'fit' parameter: ${fit}`)
+  }
 
   const transformations = [
-    ['resize', { width: +w || undefined, height: +h || undefined }]
+    ['resize', w || h ? { width: +w || undefined, height: +h || undefined, fit } : null],
+    ['blur', +b],
+    ['jpeg', q ? { quality: +q } : null]
   ];
 
   let srcImgBuffer = Buffer.from(
@@ -13,7 +25,7 @@ export default async (req, res) => {
 
   const img = await transformations.reduce(async (acc, [key, param]) => {
     const img = await acc;
-    return img[key](param);
+    return param ? img[key](param) : img;
   }, sharp(srcImgBuffer));
 
   res.end(await img.toBuffer());
